60aecd6ee0dd76576965ad7c1a8ef3b700f499bc
filipe paiva <filipe.p@soltroiavillas.com>
2017-06-19 18:47:36 +0100

    adding stuff

--- app/models/space.rb
+++ app/models/space.rb
@@ -2,4 +2,7 @@ class Space < ApplicationRecord
   belongs_to :user
 
   validates :name, :location, :user_id, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
 end
