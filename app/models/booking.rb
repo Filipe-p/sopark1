b4b4da350b0fca63531e8419a0465349974e5867
Pedro Rio <prio@teachme.site>
2017-06-19 20:43:37 +0100

    Started landing page

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,5 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :space
 
-  validates :user_id, :space_id, :start_datetime, :end_datetime, :cost presence: true
+  validates :user_id, :space_id, :start_datetime, :end_datetime, :cost, presence: true
 end
