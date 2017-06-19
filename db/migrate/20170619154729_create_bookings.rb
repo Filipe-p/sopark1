8504f453f1f3d5c2069e013dd1f07dffc681e513
filipe paiva <filipe.p@soltroiavillas.com>
2017-06-19 16:50:02 +0100

    new working database

--- db/migrate/20170619154729_create_bookings.rb
+++ db/migrate/20170619154729_create_bookings.rb
@@ -2,7 +2,7 @@ class CreateBookings < ActiveRecord::Migration[5.0]
   def change
     create_table :bookings do |t|
       t.references :user, foreign_key: true
-      t.references :space_id, foreign_key: true
+      t.references :space, foreign_key: true
       t.date :start_datetime
       t.date :end_datetime
       t.float :cost
