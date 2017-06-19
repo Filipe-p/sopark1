f0815a8bf19a3a3b56ee3d30b0a4af6e40d321f1
filipe paiva <filipe.p@soltroiavillas.com>
2017-06-19 23:26:22 +0100

    added price to spaces, started messing with the controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170619171745) do
+ActiveRecord::Schema.define(version: 20170619220545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20170619171745) do
     t.datetime "updated_at", null: false
     t.float    "latitude"
     t.float    "longitude"
+    t.float    "price"
     t.index ["user_id"], name: "index_spaces_on_user_id", using: :btree
   end
 
