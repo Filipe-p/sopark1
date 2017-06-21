360267c372d3c6b3fcaf8b6bdb9b90b01ce83ea7
Pedro Rio <prio@teachme.site>
2017-06-21 19:42:14 +0100

    Finished stripe and attachinary

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,33 +1,37 @@
 class PaymentsController < ApplicationController
-  before_action :set_booking
+  before_action :set_booking, :set_space
 
   def new
   end
 
-def create
-  customer = Stripe::Customer.create(
-    source: params[:stripeToken],
-    email:  params[:stripeEmail]
-  )
+  def create
+    customer = Stripe::Customer.create(
+      source: params[:stripeToken],
+      email:  params[:stripeEmail]
+    )
 
-  charge = Stripe::Charge.create(
-    customer:     customer.id,   # You should store this customer id and re-use it.
-    amount:       @booking.amount_cents, # or amount_pennies
-    description:  "Payment for space #{@booking.space} for booking #{@booking.id}",
-    currency:     @booking.amount.currency
-  )
+    charge = Stripe::Charge.create(
+      customer:     customer.id,   # You should store this customer id and re-use it.
+      amount:       @booking.cost_cents, # or amount_pennies
+      description:  "Payment for space #{@space.name} at soparque.pt",
+      currency:     @booking.cost.currency
+    )
 
- @booking.update(payment: charge.to_json, state: 'paid')
-  redirect_to booking_path(@booking)
+    @booking.update(payment: charge.to_json, state: 'paid')
+    redirect_to space_booking_path(@space, @booking)
 
-rescue Stripe::CardError => e
-  flash[:error] = e.message
-  redirect_to new_booking_payment_path(@booking)
-end
-
-private
-
-  def set_booking
-    @booking = Booking.where(state: 'pending').find(params[:booking_id])
+  rescue Stripe::CardError => e
+    flash[:alert] = e.message
+    redirect_to new_space_booking_payment_path(@space, @booking)
   end
+
+  private
+
+    def set_booking
+      @booking = Booking.where(state: 'pending').find(params[:booking_id])
+    end
+
+    def set_space
+      @space = Space.find(params[:space_id])
+    end
 end
